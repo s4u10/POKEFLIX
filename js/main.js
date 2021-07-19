e3fa363fdd03686c43fe597960b2100ec1753a96
s4u10 <36417793+s4u10@users.noreply.github.com>
2021-07-18 23:54:02 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function redirecionar() {
+    window.open('https://www.pokemon.com/br/pokemon-estampas-ilustradas/jogar-online/', '_blank');
+}
+
+function Infredirecionar() {
+    location.href = " inf.html"
+
+}
+
+function voltar() {
+    location.href = " index.html"
+}
